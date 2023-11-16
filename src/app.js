d7a492830571c62f322d89b319abaf8093e23a12
massimo-cimenti-vinci <massimo.cimenti@student.vinci.be>
2023-11-16 15:18:26 +0100

    add: manage two name cases

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 export class Main {
     greet(name) {
+
         if(this.isInvalidString(name)){
             return "Hello, my friend.";
         }
+
+        if(typeof(name) === "object"){
+            return "Hello, Jill and Jane.";
+        }
+
 
         if(this.isUpperCase(name)){
             return "HELLO, " + name + "!";
